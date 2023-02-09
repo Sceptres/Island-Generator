1c7121717c59227215d9b79ce6c6fdee9ef95585
Abdallah Alqashqish <alqashqa@mcmaster.ca>
2023-02-09 15:07:23 -0500

    Implement basic creation of segment wrapper class

---
+++ mesh/src/main/java/ca/mcmaster/cas/se2aa4/a2/mesh/adt/Segment.java
@@ -0,0 +1,30 @@
+package ca.mcmaster.cas.se2aa4.a2.mesh.adt;
+
+import ca.mcmaster.cas.se2aa4.a2.io.Structs;
+
+public class Segment {
+
+    private Structs.Segment segment;
+
+    public Segment() {
+        this.segment = Structs.Segment.newBuilder().build();
+    }
+
+    /**
+     *
+     * @param v1Idx Index of the first {@link Structs.Vertex} of the {@link Segment}
+     * @param v2Idx Index of the second {@link Structs.Vertex} of the {@link Segment}
+     */
+    public Segment(int v1Idx, int v2Idx) {
+        this.segment = Structs.Segment.newBuilder().setV1Idx(v1Idx).setV2Idx(v2Idx).build();
+    }
+
+    /**
+     *
+     * @param segment Create a {@link Segment} from a {@link Structs.Segment}
+     */
+    public Segment(Structs.Segment segment) {
+        this.segment = segment;
+    }
+
+}
