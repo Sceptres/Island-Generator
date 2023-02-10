48cdc3068e4cc179385fff09d9ad038a375b9164
Beshoy-Hezky <hezkyb@mcmaster.ca>
2023-02-10 13:30:51 -0500

    Start Mesh

---
+++ mesh/src/main/java/ca/mcmaster/cas/se2aa4/a2/mesh/adt/mesh/Mesh.java
@@ -0,0 +1,11 @@
+package ca.mcmaster.cas.se2aa4.a2.mesh.adt.mesh;
+
+import ca.mcmaster.cas.se2aa4.a2.io.Structs;
+import ca.mcmaster.cas.se2aa4.a2.mesh.adt.segment.Segments;
+import ca.mcmaster.cas.se2aa4.a2.mesh.adt.vertex.Vertices;
+
+public class Mesh {
+    private Vertices list_of_vertices;
+    private Segments list_of_segments;
+
+}
