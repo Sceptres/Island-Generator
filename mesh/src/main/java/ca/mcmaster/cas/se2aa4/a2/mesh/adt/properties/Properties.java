1a6d2e9d8ea55ec39b54d51aca46304a21526462
Abdallah Alqashqish <alqashqa@mcmaster.ca>
2023-02-10 13:43:46 -0500

    Add properties class to store all properties

---
+++ mesh/src/main/java/ca/mcmaster/cas/se2aa4/a2/mesh/adt/properties/Properties.java
@@ -0,0 +1,5 @@
+package ca.mcmaster.cas.se2aa4.a2.mesh.adt.properties;
+
+import ca.mcmaster.cas.se2aa4.a2.mesh.adt.datastructures.UniqueList;
+
+public class Properties extends UniqueList<Property> {}
