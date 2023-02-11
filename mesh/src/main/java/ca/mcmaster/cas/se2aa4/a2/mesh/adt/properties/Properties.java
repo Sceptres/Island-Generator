48a361278965f3f0e070f76b6b25acf7c814183c
Abdallah Alqashqish <alqashqa@mcmaster.ca>
2023-02-11 13:22:39 -0500

    Remove property with the same key before adding

--- mesh/src/main/java/ca/mcmaster/cas/se2aa4/a2/mesh/adt/properties/Properties.java
+++ mesh/src/main/java/ca/mcmaster/cas/se2aa4/a2/mesh/adt/properties/Properties.java
@@ -1,11 +1,14 @@
 package ca.mcmaster.cas.se2aa4.a2.mesh.adt.properties;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 public class Properties extends ArrayList<Property> {
     @Override
     public boolean add(Property property) {
-        super.remove(property);
+        Optional<Property> keyProperty = super.stream().filter(p -> p.getKey().equals(property.getKey())).findFirst();
+        keyProperty.ifPresent(super::remove);
+
         return super.add(property);
     }
 }
