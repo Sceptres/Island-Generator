b8d364a7ee425f11b376fc54310236f7f6061684
Abdallah Alqashqish <alqashqa@mcmaster.ca>
2023-02-13 15:38:49 -0500

    Segments can now all be converted to list of Structs.Segment

--- mesh/src/main/java/ca/mcmaster/cas/se2aa4/a2/mesh/adt/segment/Segments.java
+++ mesh/src/main/java/ca/mcmaster/cas/se2aa4/a2/mesh/adt/segment/Segments.java
@@ -1,12 +1,21 @@
 package ca.mcmaster.cas.se2aa4.a2.mesh.adt.segment;
 
+import ca.mcmaster.cas.se2aa4.a2.io.Structs;
 import ca.mcmaster.cas.se2aa4.a2.mesh.adt.datastructures.UniqueList;
+import ca.mcmaster.cas.se2aa4.a2.mesh.adt.services.Converter;
 
-public class Segments extends UniqueList<Segment> {
+import java.util.List;
+
+public class Segments extends UniqueList<Segment> implements Converter<List<Structs.Segment>> {
     @Override
     public boolean add(Segment t) {
         t.setIndex(super.size());
         t.calculateColor();
         return super.add(t);
+    }
+
+    @Override
+    public List<Structs.Segment> getConverted() {
+        return super.stream().map(Segment::getConverted).toList();
     }
 }
