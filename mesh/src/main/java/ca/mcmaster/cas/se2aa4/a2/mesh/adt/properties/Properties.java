8a30f126c790a8c6a6ab6e3ed2aef0032e478908
Abdallah Alqashqish <alqashqa@mcmaster.ca>
2023-02-13 15:40:51 -0500

    Properties can now be converted to Structs.Property

--- mesh/src/main/java/ca/mcmaster/cas/se2aa4/a2/mesh/adt/properties/Properties.java
+++ mesh/src/main/java/ca/mcmaster/cas/se2aa4/a2/mesh/adt/properties/Properties.java
@@ -1,14 +1,23 @@
 package ca.mcmaster.cas.se2aa4.a2.mesh.adt.properties;
 
+import ca.mcmaster.cas.se2aa4.a2.io.Structs;
+import ca.mcmaster.cas.se2aa4.a2.mesh.adt.services.Converter;
+
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
-public class Properties extends ArrayList<Property> {
+public class Properties extends ArrayList<Property> implements Converter<List<Structs.Property>> {
     @Override
     public boolean add(Property property) {
         Optional<Property> keyProperty = super.stream().filter(p -> p.getKey().equals(property.getKey())).findFirst();
         keyProperty.ifPresent(super::remove);
 
         return super.add(property);
+    }
+
+    @Override
+    public List<Structs.Property> getConverted() {
+        return super.stream().map(Property::getConverted).toList();
     }
 }
