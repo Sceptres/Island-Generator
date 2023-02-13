04186402bc2c08d31664cf51a66b3eee54117e71
Abdallah Alqashqish <alqashqa@mcmaster.ca>
2023-02-13 15:35:15 -0500

    Vertices can now all be converted to list of Structs.Vertex

--- mesh/src/main/java/ca/mcmaster/cas/se2aa4/a2/mesh/adt/vertex/Vertices.java
+++ mesh/src/main/java/ca/mcmaster/cas/se2aa4/a2/mesh/adt/vertex/Vertices.java
@@ -1,13 +1,21 @@
 package ca.mcmaster.cas.se2aa4.a2.mesh.adt.vertex;
 
+import ca.mcmaster.cas.se2aa4.a2.io.Structs;
 import ca.mcmaster.cas.se2aa4.a2.mesh.adt.datastructures.UniqueList;
+import ca.mcmaster.cas.se2aa4.a2.mesh.adt.services.Converter;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class Vertices extends UniqueList<Vertex> {
+public class Vertices extends UniqueList<Vertex> implements Converter<List<Structs.Vertex>> {
     @Override
     public boolean add(Vertex t) {
         t.setIndex(super.size());
         return super.add(t);
+    }
+
+    @Override
+    public List<Structs.Vertex> getConverted() {
+        return super.stream().map(Vertex::getConverted).toList();
     }
 }
