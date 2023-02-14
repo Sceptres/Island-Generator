eb2715ee3e5e7b0d165cfbb85eeb41c9264302c8
Abdallah Alqashqish <alqashqa@mcmaster.ca>
2023-02-14 08:20:28 -0500

    Add utility methods to convert lists of segments/vertices

--- mesh/src/main/java/ca/mcmaster/cas/se2aa4/a2/mesh/adt/Util.java
+++ mesh/src/main/java/ca/mcmaster/cas/se2aa4/a2/mesh/adt/Util.java
@@ -8,13 +8,11 @@ import ca.mcmaster.cas.se2aa4.a2.mesh.adt.segment.Segment;
 import ca.mcmaster.cas.se2aa4.a2.mesh.adt.segment.Segments;
 import ca.mcmaster.cas.se2aa4.a2.mesh.adt.services.IProperties;
 import ca.mcmaster.cas.se2aa4.a2.mesh.adt.vertex.Vertex;
+import ca.mcmaster.cas.se2aa4.a2.mesh.adt.vertex.Vertices;
 
 import java.awt.*;
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 import java.util.Objects;
-import java.util.stream.StreamSupport;
 
 public class Util {
     /**
@@ -50,5 +48,49 @@ public class Util {
         structProperties.forEach(p -> properties.add(new Property(p)));
 
         return properties;
+    }
+
+    /**
+     *
+     * @param segments The list of {@link Structs.Segment} to convert to {@link Segment}
+     * @param vertices The list of {@link Structs.Vertex} that match segment vertices
+     * @return A {@link Segments} list with all the {@link Segment} instances
+     */
+    public static Segments toSegments(List<? extends Structs.Segment> segments, List<? extends Structs.Vertex> vertices) {
+        Vertices verticesList = Util.toVertices(vertices);
+        return Util.toSegmentsVertex(segments, verticesList);
+    }
+
+    /**
+     *
+     * @param segments The list of {@link Structs.Segment} to convert to {@link Segment}
+     * @param vertices The list of {@link Vertex} that match segment vertices
+     * @return A {@link Segments} list with all the {@link Segment} instances
+     */
+    public static Segments toSegmentsVertex(List<? extends Structs.Segment> segments, List<? extends Vertex> vertices) {
+        Segments segmentsList = new Segments();
+
+        for (Structs.Segment structsSegment : segments) {
+            // Get vertices for this segment
+            Vertex v1 = vertices.get(structsSegment.getV1Idx());
+            Vertex v2 = vertices.get(structsSegment.getV2Idx());
+
+            // Add segment
+            segmentsList.add(new Segment(structsSegment, v1, v2));
+        }
+
+        return segmentsList;
+    }
+
+    /**
+     *
+     * @param vertices The list of {@link Structs.Vertex} instances to convert to {@link Vertex}
+     * @return The {@link Vertices} list with all the {@link Vertex} instances
+     */
+    public static Vertices toVertices(Iterable<? extends Structs.Vertex> vertices) {
+        Vertices verticesList = new Vertices();
+        vertices.forEach(v -> verticesList.add(new Vertex(v)));
+
+        return verticesList;
     }
 }
