6ea7982178332c8d2ab25ffb48c9457e7a3b683a
Abdallah Alqashqish <alqashqa@mcmaster.ca>
2023-02-19 19:51:44 -0500

    Implement mesh file input option Implement option to take the path of the mesh input file

---
+++ visualizer/src/main/java/ca/mcmaster/cas/se2aa4/a2/visualizer/cli/options/InputOption.java
@@ -0,0 +1,18 @@
+package ca.mcmaster.cas.se2aa4.a2.visualizer.cli.options;
+
+import org.apache.commons.cli.Option;
+
+/**
+ * Option that takes in the input mesh file
+ */
+public class InputOption extends Option {
+
+    public static final String OPTION_STR = "input";
+    private static final String DESCRIPTION = "Takes in the mesh file to read from.";
+
+    public InputOption() throws IllegalArgumentException {
+        super(OPTION_STR.substring(0, 2), OPTION_STR, true, DESCRIPTION);
+        super.setRequired(true);
+        super.setArgs(1);
+    }
+}
