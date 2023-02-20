972791de43b6e937ad798436208ba9fe27cb33d9
Abdallah Alqashqish <alqashqa@mcmaster.ca>
2023-02-20 12:40:28 -0500

    Implement option to choose mesh dimensions

---
+++ generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/generator/cli/options/MeshDimensionsOption.java
@@ -0,0 +1,19 @@
+package ca.mcmaster.cas.se2aa4.a2.generator.cli.options;
+
+import org.apache.commons.cli.Option;
+
+/**
+ * Option to set the dimensions of the mesh
+ */
+public class MeshDimensionsOption extends Option {
+
+    public static final String OPTION_STR = "dimension";
+    public static final String DEFAULT_VALUE = "500x500";
+    private static final String DESCRIPTION = "Sets the dimensions of the mesh. Format widthxheight. Default is 500x500.";
+
+    public MeshDimensionsOption() throws IllegalArgumentException {
+        super(OPTION_STR.substring(0, 1), OPTION_STR, true, DESCRIPTION);
+        super.setArgs(1);
+        super.setRequired(false);
+    }
+}
