c9801db5690a6401c4b08de2ee45e5c36421cea4
Abdallah Alqashqish <alqashqa@mcmaster.ca>
2023-02-20 13:36:28 -0500

    Implement relaxation level option

---
+++ generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/generator/cli/options/RelaxationLevelOption.java
@@ -0,0 +1,17 @@
+package ca.mcmaster.cas.se2aa4.a2.generator.cli.options;
+
+import org.apache.commons.cli.Option;
+
+public class RelaxationLevelOption extends Option {
+
+    public static final String OPTION_STR = "relaxationLevel";
+    public static final String DEFAULT_VALUE = "3";
+    private static final String DESCRIPTION = "Sets the relaxation level of an irregular mesh. Will be ignored by grid mesh";
+
+    public RelaxationLevelOption() throws IllegalArgumentException {
+        super("rl", OPTION_STR, true, DESCRIPTION);
+        super.setArgs(1);
+        super.setArgName("relaxation level>");
+        super.setRequired(false);
+    }
+}
