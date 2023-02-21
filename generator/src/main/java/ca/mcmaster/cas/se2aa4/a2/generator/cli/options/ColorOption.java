8594707b79e0c98b34562952e965a50a4d292cb6
Abdallah Alqashqish <alqashqa@mcmaster.ca>
2023-02-20 23:39:40 -0500

    Reformat color option decription

--- generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/generator/cli/options/ColorOption.java
+++ generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/generator/cli/options/ColorOption.java
@@ -2,15 +2,20 @@ package ca.mcmaster.cas.se2aa4.a2.generator.cli.options;
 
 import org.apache.commons.cli.Option;
 
+/**
+ * Sets the coloring of a specific object
+ */
 public class ColorOption extends Option {
 
     public static final String OPTION_STR = "color";
     public static final String DEFAULT_VALUE = "random random random";
-    private static final String DESCRIPTION = "Sets the color generation for all the elements of the mesh. " +
-            "Format: vertices segments polygons. " +
-            "General options: random, r,g,b or r,g,b,a. " +
-            "Segment options: vertices (average color of vertices). " +
-            "Polygon options: transparent, vertices (average color of vertices) or segments (average color of segments). " +
+    private static final String DESCRIPTION = "Sets the color generation for all the elements of the mesh.\n" +
+            "Format: vertices segments polygons.\n" +
+            "Vertex options: random | r,g,b | r,g,b,a.\n" +
+            "Segment options: random | r,g,b | r,g,b,a | vertices (average color of vertices).\n" +
+            "Polygon options: random | r,g,b | r,g,b,a | transparent | vertices (average color of vertices) | " +
+            "" +
+            "segments (average color of segments).\n" +
             "Default is random for all 3.";
 
     public ColorOption() throws IllegalArgumentException {
