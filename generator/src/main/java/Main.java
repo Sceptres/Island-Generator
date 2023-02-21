2fe12a88c8fe49dad6e975496bffd5ac5e2db88d
Beshoy-Hezky <hezkyb@mcmaster.ca>
2023-02-21 14:37:39 -0500

    Implement color input handling in Main

--- generator/src/main/java/Main.java
+++ generator/src/main/java/Main.java
@@ -1,28 +1,30 @@
 import ca.mcmaster.cas.se2aa4.a2.generator.DotGen;
 import ca.mcmaster.cas.se2aa4.a2.generator.cli.GeneratorInputHandler;
 import ca.mcmaster.cas.se2aa4.a2.generator.cli.exceptions.SquaresFittingException;
+import ca.mcmaster.cas.se2aa4.a2.generator.cli.options.ColorOption;
 import ca.mcmaster.cas.se2aa4.a2.generator.cli.options.MeshDimensionsOption;
 import ca.mcmaster.cas.se2aa4.a2.generator.cli.options.MeshTypeOption;
 import ca.mcmaster.cas.se2aa4.a2.generator.cli.options.SquareSizeOption;
+import ca.mcmaster.cas.se2aa4.a2.generator.coloring.ColorGenerator;
+import ca.mcmaster.cas.se2aa4.a2.generator.coloring.generators.RandomColorGenerator;
+import ca.mcmaster.cas.se2aa4.a2.generator.coloring.generators.SetColorGenerator;
+import ca.mcmaster.cas.se2aa4.a2.generator.coloring.generators.polygoncolor.PolygonSegmentColorGenerator;
+import ca.mcmaster.cas.se2aa4.a2.generator.coloring.generators.polygoncolor.PolygonTransparentColorGenerator;
+import ca.mcmaster.cas.se2aa4.a2.generator.coloring.generators.polygoncolor.PolygonVertexColorGenerator;
+import ca.mcmaster.cas.se2aa4.a2.generator.coloring.generators.segmentcolor.SegmentColorGenerator;
 import ca.mcmaster.cas.se2aa4.a2.generator.generator.MeshGenerator;
 import ca.mcmaster.cas.se2aa4.a2.generator.generator.generators.GridMeshGenerator;
 import ca.mcmaster.cas.se2aa4.a2.io.MeshFactory;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs.Mesh;
+import ca.mcmaster.cas.se2aa4.a2.mesh.adt.Util;
 import ca.mcmaster.cas.se2aa4.a2.mesh.cli.InputHandler;
 import ca.mcmaster.cas.se2aa4.a2.mesh.cli.options.OutputOption;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        InputHandler handler = GeneratorInputHandler.getInputHandler(args);
-
-        DotGen generator = new DotGen(getMeshGenerator(handler));
-        Mesh myMesh = generator.generate();
-        MeshFactory factory = new MeshFactory();
-        factory.write(myMesh, handler.getOptionValue(GeneratorInputHandler.getGeneratorOption(OutputOption.OPTION_STR)));
-    }
 
     /**
      *
@@ -37,12 +39,13 @@ public class Main {
 
         // Get mesh dimensions
         int[] meshDimensions = getMeshDimensions(handler);
+        ColorGenerator[] generators = getColorGenerator(handler);
 
         if(meshType.equals("grid")) {
             double squareSize = getGridMeshSquareSize(handler);
 
             try {
-                return new GridMeshGenerator(meshDimensions[0], meshDimensions[1], squareSize);
+                return new GridMeshGenerator(generators, meshDimensions[0], meshDimensions[1], squareSize);
             } catch (SquaresFittingException e) {
                 System.out.printf(
                         "Not all squares of size %.2f can fit in %dx%d\n",
@@ -118,4 +121,126 @@ public class Main {
         return Double.parseDouble(squareSizeInput);
     }
 
+    /**
+     *
+     * @param handler The {@link InputHandler} to extract color data from
+     * @return An array with the vertex, segments, and polygon color generation methods in order
+     */
+    private static ColorGenerator[] getColorGenerator(InputHandler handler) {
+        String[] values = handler.getOptionValues(
+                GeneratorInputHandler.getGeneratorOption(ColorOption.OPTION_STR),
+                ColorOption.DEFAULT_VALUE
+        );
+
+        // Get element color generators
+        ColorGenerator vertexColorGenerator = getVertexColorGenerator(handler, values[0]);
+        ColorGenerator segmentColorGenerator = getSegmentColorGenerator(handler, values[1]);
+        ColorGenerator polygonColorGenerator = getPolygonColorGenerator(handler, values[2]);
+
+        return new ColorGenerator[]{vertexColorGenerator, segmentColorGenerator, polygonColorGenerator};
+    }
+
+    /**
+     *
+     * @param input The passed in CMD input by the user
+     * @return The color generator matching the given input. Only covers common cases: random | r,g,b | r,g,b,a
+     */
+    private static ColorGenerator getGeneralColorGenerator(InputHandler handler, String input) {
+        try {
+            if (input.equals("random"))
+                return new RandomColorGenerator();
+            else if (input.matches("[0-9]+,[0-9]+,[0-9]+")) {
+                // Convert RGB to RGBA
+                String rgbaStr = input+",255";
+                String[] rgbStr = rgbaStr.split(",");
+                return new SetColorGenerator(Util.getColorFromStr(rgbStr));
+            } else if (input.matches("[0-9]+,[0-9]+,[0-9]+,[0-9]+")) {
+                String[] rgbaStr = input.split(",");
+                return new SetColorGenerator(Util.getColorFromStr(rgbaStr));
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println("Invalid RGB/RGBA input.");
+            handler.printHelp();
+            System.exit(1);
+        }
+
+        return null;
+    }
+
+    /**
+     *
+     * @param handler The {@link InputHandler} to print out any errors with input
+     * @param input The input for the vertex color generation
+     * @return The {@link ColorGenerator} for the vertices
+     */
+    private static ColorGenerator getVertexColorGenerator(InputHandler handler, String input) {
+        ColorGenerator generator = getGeneralColorGenerator(handler, input);
+
+        if(Objects.isNull(generator)) { // Nothing chosen from general options?
+            System.out.printf("Invalid color generation method %s for vertex!", input);
+            handler.printHelp();
+            System.exit(1);
+        }
+
+        return generator;
+    }
+
+    /**
+     *
+     * @param handler The {@link InputHandler} to print out any errors with input
+     * @param input The input for the segment color generation
+     * @return The {@link ColorGenerator} for the segments
+     */
+    private static ColorGenerator getSegmentColorGenerator(InputHandler handler, String input) {
+        ColorGenerator generator = getGeneralColorGenerator(handler, input);
+
+        if(Objects.isNull(generator)) {  // Nothing chosen from general options?
+            if(input.equals("vertices")) { // User request segments color to be average color of vertices?
+                generator = new SegmentColorGenerator();
+            } else {
+                System.out.printf("Invalid color generation method %s for segment!", input);
+                handler.printHelp();
+                System.exit(1);
+            }
+        }
+
+        return generator;
+    }
+
+    /**
+     *
+     * @param handler The {@link InputHandler} to print out any errors with input
+     * @param input The input for the polygon color generation
+     * @return The {@link ColorGenerator} for the polygons
+     */
+    private static ColorGenerator getPolygonColorGenerator(InputHandler handler, String input) {
+        ColorGenerator generator = getGeneralColorGenerator(handler, input);
+
+        if(Objects.isNull(generator)) {  // Nothing chosen from general options?
+            switch (input) {
+                case "vertices" -> // User request polygon color to be average color of vertices?
+                        generator = new PolygonVertexColorGenerator();
+                case "segments" ->  // User request polygon color to be average color of segments?
+                        generator = new PolygonSegmentColorGenerator();
+                case "transparent" -> // User request polygon color to be transparent?
+                        generator = new PolygonTransparentColorGenerator();
+                default -> {
+                    System.out.printf("Invalid color generation method %s for polygon!", input);
+                    handler.printHelp();
+                    System.exit(1);
+                }
+            }
+        }
+
+        return generator;
+    }
+
+    public static void main(String[] args) throws IOException {
+        InputHandler handler = GeneratorInputHandler.getInputHandler(args);
+
+        DotGen generator = new DotGen(getMeshGenerator(handler));
+        Mesh myMesh = generator.generate();
+        MeshFactory factory = new MeshFactory();
+        factory.write(myMesh, handler.getOptionValue(GeneratorInputHandler.getGeneratorOption(OutputOption.OPTION_STR)));
+    }
 }
