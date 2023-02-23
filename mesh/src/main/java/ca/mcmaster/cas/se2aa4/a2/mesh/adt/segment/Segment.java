e377badbf935ef2a81c61c3d681cdb66de69024f
Abdallah Alqashqish <alqashqa@mcmaster.ca>
2023-02-23 18:28:18 -0500

    Implement method to get common vertex between segments

--- mesh/src/main/java/ca/mcmaster/cas/se2aa4/a2/mesh/adt/segment/Segment.java
+++ mesh/src/main/java/ca/mcmaster/cas/se2aa4/a2/mesh/adt/segment/Segment.java
@@ -150,8 +150,21 @@ public class Segment implements Indexable,Thickenable, IProperties, Renderable, 
      * @return True if this segment and the given segment share a vertex meaning they are connected. False otherwise.
      */
     public boolean shareVertex(Segment segment) {
-        return this.v1.equals(segment.v1) || this.v1.equals(segment.v2) ||
-                this.v2.equals(segment.v1) || this.v2.equals(segment.v2);
+        return Objects.nonNull(this.getSharedVertex(segment));
+    }
+
+    /**
+     *
+     * @param segment The segment to get shared {@link Vertex} from
+     * @return The {@link Vertex} that is shared between this segment and the given one
+     */
+    public Vertex getSharedVertex(Segment segment) {
+        if(this.v1.equals(segment.v1) || this.v1.equals(segment.v2))
+            return this.v1;
+        else if(this.v2.equals(segment.v2) || this.v2.equals(segment.v1))
+            return this.v2;
+        else
+            return null;
     }
 
     @Override
