81635e9d097beb4d511423b0670d3af8272d261f
Abdallah Alqashqish <alqashqa@mcmaster.ca>
2023-02-24 16:54:46 -0500

    Mesh generators have to seperately set polygon neighbors

--- generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/generator/DotGen.java
+++ generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/generator/DotGen.java
@@ -1,12 +1,8 @@
 package ca.mcmaster.cas.se2aa4.a2.generator;
 
-import ca.mcmaster.cas.se2aa4.a2.generator.coloring.ColorGenerator;
-import ca.mcmaster.cas.se2aa4.a2.generator.coloring.generators.RandomColorGenerator;
 import ca.mcmaster.cas.se2aa4.a2.generator.mesh.generator.MeshGenerator;
-import ca.mcmaster.cas.se2aa4.a2.generator.mesh.generator.generators.IrregularMeshGenerator;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs;
 import ca.mcmaster.cas.se2aa4.a2.mesh.adt.mesh.Mesh;
-import ca.mcmaster.cas.se2aa4.a2.mesh.adt.polygon.Polygon;
 
 public class DotGen {
 
@@ -32,13 +28,6 @@ public class DotGen {
         mesh.setDimension(dimensions);
         // Generate mesh
         this.generator.generate(mesh);
-
-        // Match neighboring polygons
-        for(Polygon p1 : mesh.getPolygons()) {
-            for(Polygon p2 : mesh.getPolygons()) {
-                p1.addNeighbor(p2);
-            }
-        }
 
         return mesh.getConverted();
     }
