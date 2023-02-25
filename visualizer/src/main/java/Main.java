2e4b66bfa35a4501e5653f0389eab917cb0b9b10
Omar Shehada <awadomar24@gmail.com>
2023-02-24 19:36:17 -0500

    Move input handling functions from main to input handler

--- visualizer/src/main/java/Main.java
+++ visualizer/src/main/java/Main.java
@@ -22,8 +22,8 @@ public class Main {
         InputHandler handler = VisualizerInputHandler.getInputHandler(args);
 
         // Extracting command line parameters
-        String input = getInputMesh(handler);
-        String output = getOutputFile(handler);
+        String input = VisualizerInputHandler.getInputMesh(handler);
+        String output = VisualizerInputHandler.getOutputFile(handler);
         // Getting width and height for the canvas
         Structs.Mesh aMesh = new MeshFactory().read(input);
 
@@ -48,37 +48,4 @@ public class Main {
         dumper.dump(aMesh);
     }
 
-    /**
-     *
-     * @param handler The {@link InputHandler} to get the data from
-     * @return The passed in data for the input mesh file path
-     */
-    private static String getInputMesh(InputHandler handler) {
-        String value = handler.getOptionValue(VisualizerInputHandler.getVisualizerOption(InputOption.OPTION_STR));
-
-        if(!Files.exists(Path.of(value))) { // Does this file not exist?
-            String message = String.format("Cannot find %s. Please try again!\n", value);
-            handler.printHelp(message);
-        } else if(!value.endsWith(".mesh")) { // Is the given file not a mesh?
-            String message = String.format("%s is not a mesh file! Please insert correct file format.\n", value);
-            handler.printHelp(message);
-        }
-
-        return value;
-    }
-
-    /**
-     *
-     * @param handler The {@link InputHandler} to get the data from
-     * @return The path of the file to output the mesh to
-     */
-    private static String getOutputFile(InputHandler handler) {
-        String value = handler.getOptionValue(VisualizerInputHandler.getVisualizerOption(OutputOption.OPTION_STR));
-
-        if(!value.endsWith(".svg")){
-            handler.printHelp("The given output file is not an svg!");
-        }
-
-        return value;
-    }
 }
--- visualizer/src/main/java/ca/mcmaster/cas/se2aa4/a2/visualizer/cli/VisualizerInputHandler.java
+++ visualizer/src/main/java/ca/mcmaster/cas/se2aa4/a2/visualizer/cli/VisualizerInputHandler.java
@@ -6,6 +6,8 @@ import ca.mcmaster.cas.se2aa4.a2.visualizer.cli.options.DebugOption;
 import ca.mcmaster.cas.se2aa4.a2.visualizer.cli.options.InputOption;
 import org.apache.commons.cli.Option;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Map;
 
 public class VisualizerInputHandler {
@@ -34,5 +36,39 @@ public class VisualizerInputHandler {
      */
     public static InputHandler getInputHandler(String[] args) {
         return new InputHandler(args, VISUALIZER_OPTIONS);
+    }
+
+    /**
+     *
+     * @param handler The {@link InputHandler} to get the data from
+     * @return The passed in data for the input mesh file path
+     */
+    public static String getInputMesh(InputHandler handler) {
+        String value = handler.getOptionValue(VisualizerInputHandler.getVisualizerOption(InputOption.OPTION_STR));
+
+        if(!Files.exists(Path.of(value))) { // Does this file not exist?
+            String message = String.format("Cannot find %s. Please try again!\n", value);
+            handler.printHelp(message);
+        } else if(!value.endsWith(".mesh")) { // Is the given file not a mesh?
+            String message = String.format("%s is not a mesh file! Please insert correct file format.\n", value);
+            handler.printHelp(message);
+        }
+
+        return value;
+    }
+
+    /**
+     *
+     * @param handler The {@link InputHandler} to get the data from
+     * @return The path of the file to output the mesh to
+     */
+    public static String getOutputFile(InputHandler handler) {
+        String value = handler.getOptionValue(VisualizerInputHandler.getVisualizerOption(OutputOption.OPTION_STR));
+
+        if(!value.endsWith(".svg")){
+            handler.printHelp("The given output file is not an svg!");
+        }
+
+        return value;
     }
 }
