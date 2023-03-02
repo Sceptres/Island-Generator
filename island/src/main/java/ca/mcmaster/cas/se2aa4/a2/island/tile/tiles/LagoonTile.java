5ef6d7be2b0adc8a731114bc66530166103e798a
Beshoy-Hezky <hezkyb@mcmaster.ca>
2023-03-02 18:58:14 -0500

    F02: Implement lagoon tile

---
+++ island/src/main/java/ca/mcmaster/cas/se2aa4/a2/island/tile/tiles/LagoonTile.java
@@ -0,0 +1,15 @@
+package ca.mcmaster.cas.se2aa4.a2.island.tile.tiles;
+
+import ca.mcmaster.cas.se2aa4.a2.island.tile.Tile;
+import ca.mcmaster.cas.se2aa4.a2.island.tile.TileColors;
+import ca.mcmaster.cas.se2aa4.a2.island.tile.TileType;
+import ca.mcmaster.cas.se2aa4.a2.mesh.adt.polygon.Polygon;
+
+
+public class LagoonTile extends Tile {
+    public LagoonTile(Polygon polygon){
+        super(polygon);
+        super.type = TileType.WATER_TILE;
+        super.polygon.setColor(TileColors.LAGOON);
+    }
+}
