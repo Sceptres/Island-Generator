fa9c7846d2e4b85d02d6971f14cfe843edb4a906
Abdallah Alqashqish <alqashqa@mcmaster.ca>
2023-03-05 13:34:41 -0500

    F04: Implement the ocean tile type

--- island/src/main/java/ca/mcmaster/cas/se2aa4/a2/island/tile/TileType.java
+++ island/src/main/java/ca/mcmaster/cas/se2aa4/a2/island/tile/TileType.java
@@ -4,11 +4,13 @@ import ca.mcmaster.cas.se2aa4.a2.island.tile.color.TileColorGenerator;
 import ca.mcmaster.cas.se2aa4.a2.island.tile.color.colors.BeachColorGenerator;
 import ca.mcmaster.cas.se2aa4.a2.island.tile.color.colors.LagoonColorGenerator;
 import ca.mcmaster.cas.se2aa4.a2.island.tile.color.colors.LandColorGenerator;
+import ca.mcmaster.cas.se2aa4.a2.island.tile.color.colors.OceanColorGenerator;
 
 public enum TileType {
     BEACH_TILE(new BeachColorGenerator()),
     LAND_TILE(new LandColorGenerator()),
-    LAGOON_TILE(new LagoonColorGenerator());
+    LAGOON_TILE(new LagoonColorGenerator()),
+    OCEAN_TILE(new OceanColorGenerator());
 
 
     private final TileColorGenerator generator;
---
+++ island/src/main/java/ca/mcmaster/cas/se2aa4/a2/island/tile/color/colors/OceanColorGenerator.java
@@ -0,0 +1,13 @@
+package ca.mcmaster.cas.se2aa4.a2.island.tile.color.colors;
+
+import ca.mcmaster.cas.se2aa4.a2.island.tile.TileColors;
+import ca.mcmaster.cas.se2aa4.a2.island.tile.color.TileColorGenerator;
+
+import java.awt.*;
+
+public class OceanColorGenerator implements TileColorGenerator {
+    @Override
+    public Color generateColor() {
+        return TileColors.OCEAN;
+    }
+}
