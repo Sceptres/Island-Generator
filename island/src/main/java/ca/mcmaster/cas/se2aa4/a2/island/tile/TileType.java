6eb1a5272d459b1e39738be916bfb430c91bb267
Abdallah Alqashqish <alqashqa@mcmaster.ca>
2023-03-05 13:28:51 -0500

    F03: Implement beach tile type

--- island/src/main/java/ca/mcmaster/cas/se2aa4/a2/island/tile/TileType.java
+++ island/src/main/java/ca/mcmaster/cas/se2aa4/a2/island/tile/TileType.java
@@ -1,9 +1,12 @@
 package ca.mcmaster.cas.se2aa4.a2.island.tile;
 
 import ca.mcmaster.cas.se2aa4.a2.island.tile.color.TileColorGenerator;
+import ca.mcmaster.cas.se2aa4.a2.island.tile.color.colors.BeachColorGenerator;
 
 public enum TileType {
-    ;
+    BEACH_TILE(new BeachColorGenerator());
+
+    
     private final TileColorGenerator generator;
 
     TileType(TileColorGenerator generator) {
