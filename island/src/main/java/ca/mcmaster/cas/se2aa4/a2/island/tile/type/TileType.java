25f97708cc21c00606bd08b4f8a570594452719e
Abdallah Alqashqish <alqashqa@mcmaster.ca>
2023-03-06 19:20:31 -0500

    F01: Implement tile groups Implement the different tile groups. Water, Land, and None

--- island/src/main/java/ca/mcmaster/cas/se2aa4/a2/island/tile/type/TileType.java
+++ island/src/main/java/ca/mcmaster/cas/se2aa4/a2/island/tile/type/TileType.java
@@ -1,4 +1,4 @@
-package ca.mcmaster.cas.se2aa4.a2.island.tile;
+package ca.mcmaster.cas.se2aa4.a2.island.tile.type;
 
 import ca.mcmaster.cas.se2aa4.a2.island.tile.color.TileColorGenerator;
 import ca.mcmaster.cas.se2aa4.a2.island.tile.color.colors.BeachColorGenerator;
@@ -7,16 +7,26 @@ import ca.mcmaster.cas.se2aa4.a2.island.tile.color.colors.LandColorGenerator;
 import ca.mcmaster.cas.se2aa4.a2.island.tile.color.colors.OceanColorGenerator;
 
 public enum TileType {
-    BEACH_TILE(new BeachColorGenerator()),
-    LAND_TILE(new LandColorGenerator()),
-    LAGOON_TILE(new LagoonColorGenerator()),
-    OCEAN_TILE(new OceanColorGenerator());
+    BEACH_TILE(TileGroup.LAND, new BeachColorGenerator()),
+    LAND_TILE(TileGroup.LAND, new LandColorGenerator()),
+    LAGOON_TILE(TileGroup.WATER, new LagoonColorGenerator()),
+    OCEAN_TILE(TileGroup.WATER, new OceanColorGenerator());
 
 
+    private final TileGroup group;
     private final TileColorGenerator generator;
 
-    TileType(TileColorGenerator generator) {
+    TileType(TileGroup group, TileColorGenerator generator) {
+        this.group = group;
         this.generator = generator;
+    }
+
+    /**
+     *
+     * @return The {@link TileGroup} that this tile type belongs to
+     */
+    public TileGroup getGroup() {
+        return this.group;
     }
 
     /**
