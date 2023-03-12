4b18f013387f5a724722169bc933243d8a2a8d9f
Omar Shehada <awadomar24@gmail.com>
2023-03-12 19:59:47 -0400

    Initial input parsing

--- island/src/main/java/Main.java
+++ island/src/main/java/Main.java
@@ -1,12 +1,22 @@
+import ca.mcmaster.cas.se2aa4.a2.island.cli.IslandInputHandler;
 import ca.mcmaster.cas.se2aa4.a2.island.io.MeshReader;
 import ca.mcmaster.cas.se2aa4.a2.island.io.MeshWriter;
 import ca.mcmaster.cas.se2aa4.a2.island.tile.Tile;
 import ca.mcmaster.cas.se2aa4.a2.mesh.adt.mesh.Mesh;
+import ca.mcmaster.cas.se2aa4.a2.mesh.cli.InputHandler;
 
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+
+        InputHandler handler = IslandInputHandler.getInputHandler(args);
+
+        String input = IslandInputHandler.getInputMesh(handler);
+        String output = IslandInputHandler.getOutputFile(handler);
+
+        String mode = IslandInputHandler.getIslandMode(handler);
+
         MeshReader meshReader = new MeshReader(args[0]);
         Mesh mesh = meshReader.getMesh();
 
---
+++ island/src/main/java/ca/mcmaster/cas/se2aa4/a2/island/cli/options/InputOption.java
@@ -0,0 +1,19 @@
+package ca.mcmaster.cas.se2aa4.a2.island.cli.options;
+
+import org.apache.commons.cli.Option;
+
+/**
+ * Option that takes in the input mesh file
+ */
+public class InputOption extends Option {
+
+    public static final String OPTION_STR = "input";
+    private static final String DESCRIPTION = "Takes in the mesh file to read from.";
+
+    public InputOption() throws IllegalArgumentException {
+        super(OPTION_STR.substring(0, 1), OPTION_STR, true, DESCRIPTION);
+        super.setRequired(true);
+        super.setArgName("input file>");
+        super.setArgs(1);
+    }
+}
