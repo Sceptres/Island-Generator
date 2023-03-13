6bd4a94ba43868900e99eea6276082b81d1b42b5
Abdallah Alqashqish <alqashqa@mcmaster.ca>
2023-03-13 15:51:24 -0400

    F08: Use circles in lagoon generation

--- island/src/main/java/ca/mcmaster/cas/se2aa4/a2/island/generator/generators/LagoonIslandGenerator.java
+++ island/src/main/java/ca/mcmaster/cas/se2aa4/a2/island/generator/generators/LagoonIslandGenerator.java
@@ -2,7 +2,7 @@ package ca.mcmaster.cas.se2aa4.a2.island.generator.generators;
 
 import ca.mcmaster.cas.se2aa4.a2.island.generator.AbstractIslandGenerator;
 import ca.mcmaster.cas.se2aa4.a2.island.geometry.Shape;
-import ca.mcmaster.cas.se2aa4.a2.island.geometry.shapes.Oval;
+import ca.mcmaster.cas.se2aa4.a2.island.geometry.shapes.Circle;
 import ca.mcmaster.cas.se2aa4.a2.island.tile.Tile;
 import ca.mcmaster.cas.se2aa4.a2.island.tile.type.TileGroup;
 import ca.mcmaster.cas.se2aa4.a2.island.tile.type.TileType;
@@ -20,8 +20,8 @@ public class LagoonIslandGenerator extends AbstractIslandGenerator {
 
         // Create circles
         double diagonalLength = Math.hypot(meshDimension[0], meshDimension[1]);
-        Shape circle = new Oval(diagonalLength/16f, diagonalLength/8f, meshCenter);
-        Shape circle1 = new Oval(diagonalLength/8f, diagonalLength/4f, meshCenter);
+        Shape circle = new Circle(diagonalLength/8f, meshCenter);
+        Shape circle1 = new Circle(diagonalLength/4f, meshCenter);
 
         // Make water tiles
         tiles.stream().filter(circle::contains).forEach(t -> t.setType(TileType.LAGOON_TILE));
