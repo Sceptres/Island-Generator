6e15c99dae402847599635899d63490c4b8cd4c9
Abdallah Alqashqish <alqashqa@mcmaster.ca>
2023-03-13 22:15:50 -0400

    F08: Add oval testing

--- island/src/test/java/ca/mcmaster/cas/se2aa4/a2/island/GeometryTest.java
+++ island/src/test/java/ca/mcmaster/cas/se2aa4/a2/island/GeometryTest.java
@@ -1,6 +1,8 @@
 package ca.mcmaster.cas.se2aa4.a2.island;
 
+import ca.mcmaster.cas.se2aa4.a2.island.geometry.Shape;
 import ca.mcmaster.cas.se2aa4.a2.island.geometry.shapes.Circle;
+import ca.mcmaster.cas.se2aa4.a2.island.geometry.shapes.Oval;
 import ca.mcmaster.cas.se2aa4.a2.mesh.adt.vertex.Vertex;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -13,20 +15,32 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @Testable
 public class GeometryTest {
 
-    private Circle circle;
+    private Vertex center;
 
     @BeforeEach
     public void beforeTest() {
-        Vertex center = new Vertex(100, 100);
-        this.circle = new Circle(100, center);
+        this.center = new Vertex(100, 100);
     }
 
     @Test
-    public void containsTest() {
+    public void circleTest() {
+        Shape circle = new Circle(100, this.center);
+
         Vertex v1 = new Vertex(50, 50);
         Vertex v2 = new Vertex(0, 0);
 
-        assertTrue(this.circle.contains(v1));
-        assertFalse(this.circle.contains(v2));
+        assertTrue(circle.contains(v1));
+        assertFalse(circle.contains(v2));
+    }
+
+    @Test
+    public void ovalTest() {
+        Shape circle = new Oval(50, 100, this.center);
+
+        Vertex v1 = new Vertex(120, 120);
+        Vertex v2 = new Vertex(0, 0);
+
+        assertTrue(circle.contains(v1));
+        assertFalse(circle.contains(v2));
     }
 }
