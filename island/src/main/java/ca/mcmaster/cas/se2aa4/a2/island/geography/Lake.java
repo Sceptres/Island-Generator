cc86aea9ffb583272c752342eab2449adb4d7900
Abdallah Alqashqish <alqashqa@mcmaster.ca>
2023-03-21 00:14:45 -0400

    F09: Lake generation method is static

--- island/src/main/java/ca/mcmaster/cas/se2aa4/a2/island/geography/Lake.java
+++ island/src/main/java/ca/mcmaster/cas/se2aa4/a2/island/geography/Lake.java
@@ -8,20 +8,21 @@ import java.util.List;
 import java.util.Random;
 
 public class Lake extends TiledGeography {
-    public Lake(Tile start) {
-        super(TileType.LAND_WATER_TILE);
-        super.addTile(start);
-    }
-
     /**
      *
      * @param random The random instance to use in finding next tile
      * @return The next {@link Tile} of the lake
      */
-    public Tile getNextTile(Random random) {
-        Tile tile = super.tiles.get(super.tiles.size()-1);
+    public static Tile getNextTile(Lake lake, Random random) {
+        List<Tile> tiles = lake.getTiles();
+        Tile tile = tiles.get(tiles.size()-1);
         List<Tile> neighbors = tile.getNeighbors();
         return neighbors.get(random.nextInt(neighbors.size()));
+    }
+
+    public Lake(Tile start) {
+        super(TileType.LAND_WATER_TILE);
+        super.addTile(start);
     }
 
     @Override
