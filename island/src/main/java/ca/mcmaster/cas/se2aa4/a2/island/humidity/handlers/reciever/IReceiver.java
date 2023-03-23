dc6fbbe77f4f91d341debc5e326a6dd57ba78ca8
Beshoy-Hezky <hezkyb@mcmaster.ca>
2023-03-23 18:33:00 -0400

    F14: Implement receiver class

--- island/src/main/java/ca/mcmaster/cas/se2aa4/a2/island/humidity/handlers/reciever/IReceiver.java
+++ island/src/main/java/ca/mcmaster/cas/se2aa4/a2/island/humidity/handlers/reciever/IReceiver.java
@@ -1,6 +1,9 @@
 package ca.mcmaster.cas.se2aa4.a2.island.humidity.handlers.reciever;
 
+import ca.mcmaster.cas.se2aa4.a2.island.humidity.IHumidity;
+
 public interface IReceiver {
 
-    void receiveHumidity(double humidity);
+    void receiveHumidity(IHumidity h, float humidity);
+
 }
