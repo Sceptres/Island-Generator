3488c8e96d5f040e01ccbaf4fa45e7038c00ae13
Abdallah Alqashqish <alqashqa@mcmaster.ca>
2023-03-23 18:28:28 -0400

    F14: Correctly implement contract

--- island/src/main/java/ca/mcmaster/cas/se2aa4/a2/island/humidity/profiles/HumidityProfile.java
+++ island/src/main/java/ca/mcmaster/cas/se2aa4/a2/island/humidity/profiles/HumidityProfile.java
@@ -5,14 +5,21 @@ import ca.mcmaster.cas.se2aa4.a2.island.humidity.IHumidity;
 public class HumidityProfile implements IHumidity {
 
     private double humidity;
+
+
     @Override
     public double getHumidity() {
-        return 0;
+        return this.humidity;
     }
 
     @Override
     public void setHumidity(double humidity) {
         this.humidity = humidity;
+    }
+
+    @Override
+    public void giveHumidity(IHumidity h) {
+        throw new UnsupportedOperationException();
     }
 
 }
