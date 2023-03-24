e45b068fe4e81db6c2314873c90bb883f84808ce
Abdallah Alqashqish <alqashqa@mcmaster.ca>
2023-03-24 17:04:43 -0400

    F11: Set correct river color

--- island/src/main/java/ca/mcmaster/cas/se2aa4/a2/island/path/PathColor.java
+++ island/src/main/java/ca/mcmaster/cas/se2aa4/a2/island/path/PathColor.java
@@ -3,7 +3,6 @@ package ca.mcmaster.cas.se2aa4.a2.island.path;
 import java.awt.*;
 
 public class PathColor {
-    public static final Color RIVER_COLOR = Color.RED;
-    //new Color(15, 92, 134);
+    public static final Color RIVER_COLOR = new Color(15, 92, 134);
     public static final Color TRANSPARENT_COLOR = new Color(0, 0, 0, 0);
 }
