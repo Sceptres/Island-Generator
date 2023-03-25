fb5465667eea9067e1ecbf011a1ad8ccfdd53539
Abdallah Alqashqish <alqashqa@mcmaster.ca>
2023-03-25 13:37:46 -0400

    F11: Make rivers have the same color as lakes

--- island/src/main/java/ca/mcmaster/cas/se2aa4/a2/island/path/PathColor.java
+++ island/src/main/java/ca/mcmaster/cas/se2aa4/a2/island/path/PathColor.java
@@ -1,8 +1,10 @@
 package ca.mcmaster.cas.se2aa4.a2.island.path;
 
+import ca.mcmaster.cas.se2aa4.a2.island.tile.TileColors;
+
 import java.awt.*;
 
 public class PathColor {
-    public static final Color RIVER_COLOR = new Color(15, 92, 134);
+    public static final Color RIVER_COLOR = TileColors.LAND_WATER;
     public static final Color TRANSPARENT_COLOR = new Color(0, 0, 0, 0);
 }
