8486d5e87edd6870773bb9f46a3fd184e84a1100
Abdallah Alqashqish <alqashqa@mcmaster.ca>
2023-03-26 08:40:03 -0400

    F13: Optimize imports

--- island/src/main/java/ca/mcmaster/cas/se2aa4/a2/island/humidity/soil/profiles/WetSoilAbsorption.java
+++ island/src/main/java/ca/mcmaster/cas/se2aa4/a2/island/humidity/soil/profiles/WetSoilAbsorption.java
@@ -1,9 +1,7 @@
 package ca.mcmaster.cas.se2aa4.a2.island.humidity.soil.profiles;
 
 import ca.mcmaster.cas.se2aa4.a2.island.humidity.handlers.reciever.HumidityReceiver;
-import ca.mcmaster.cas.se2aa4.a2.island.humidity.handlers.reciever.IHumidityReceiver;
 import ca.mcmaster.cas.se2aa4.a2.island.humidity.handlers.transmitter.HumidityTransmitter;
-import ca.mcmaster.cas.se2aa4.a2.island.humidity.handlers.transmitter.IHumidityTransmitter;
 import ca.mcmaster.cas.se2aa4.a2.island.humidity.soil.AbstractSoilAbsorption;
 
 public class WetSoilAbsorption extends AbstractSoilAbsorption {
