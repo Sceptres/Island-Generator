219944ddf41a289bda82830b5f528d54165192d8
Abdallah Alqashqish <alqashqa@mcmaster.ca>
2023-03-25 22:37:05 -0400

    F14: Implement ocean humidity

--- island/src/main/java/ca/mcmaster/cas/se2aa4/a2/island/geography/Ocean.java
+++ island/src/main/java/ca/mcmaster/cas/se2aa4/a2/island/geography/Ocean.java
@@ -1,17 +1,37 @@
 package ca.mcmaster.cas.se2aa4.a2.island.geography;
 
+import ca.mcmaster.cas.se2aa4.a2.island.humidity.IHumidity;
 import ca.mcmaster.cas.se2aa4.a2.island.tile.Tile;
+import ca.mcmaster.cas.se2aa4.a2.island.tile.type.TileGroup;
 import ca.mcmaster.cas.se2aa4.a2.island.tile.type.TileType;
 
 import java.util.List;
 
-public class Ocean extends TiledGeography {
+public class Ocean extends TiledGeography implements IHumidity {
     public Ocean() {
         super(TileType.OCEAN_TILE);
     }
 
     @Override
     public List<Tile> getNeighbors() {
-        return List.of();
+        return super.tiles.stream()
+                .flatMap(t -> t.getNeighbors().stream())
+                .filter(t -> t.getType().getGroup() == TileGroup.LAND)
+                .toList();
+    }
+
+    @Override
+    public float getHumidity() {
+        return -100;
+    }
+
+    @Override
+    public void setHumidity(float humidity) {}
+
+    @Override
+    public void giveHumidity(IHumidity h) {
+        float oceanHumidity = this.getHumidity();
+        float oldHumidity = h.getHumidity();
+        h.setHumidity(oldHumidity + oceanHumidity);
     }
 }
