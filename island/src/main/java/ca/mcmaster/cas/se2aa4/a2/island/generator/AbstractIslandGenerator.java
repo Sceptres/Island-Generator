f7aefc754b12a7ebd00bedcc482faf8bf69c17d5
Beshoy-Hezky <hezkyb@mcmaster.ca>
2023-03-26 15:50:34 -0400

    code clean up

---
+++ island/src/main/java/ca/mcmaster/cas/se2aa4/a2/island/generator/AbstractIslandGenerator.java
@@ -0,0 +1,143 @@
+package ca.mcmaster.cas.se2aa4.a2.island.generator;
+
+import ca.mcmaster.cas.se2aa4.a2.island.elevation.altimetry.AltimeterProfile;
+import ca.mcmaster.cas.se2aa4.a2.island.geography.Land;
+import ca.mcmaster.cas.se2aa4.a2.island.geography.Ocean;
+import ca.mcmaster.cas.se2aa4.a2.island.geography.River;
+import ca.mcmaster.cas.se2aa4.a2.island.geography.generator.generators.RiverGenerator;
+import ca.mcmaster.cas.se2aa4.a2.island.geometry.Shape;
+import ca.mcmaster.cas.se2aa4.a2.island.mesh.IslandMesh;
+import ca.mcmaster.cas.se2aa4.a2.island.tile.Tile;
+import ca.mcmaster.cas.se2aa4.a2.island.tile.type.TileGroup;
+
+import java.util.List;
+import java.util.Random;
+
+public abstract class AbstractIslandGenerator implements IslandGenerator {
+
+    private final Land land;
+    private final Ocean ocean;
+    private final Random rand;
+    private final long seed;
+    private final int numLakes;
+    private final int numRivers;
+    private final int numAquifers;
+    private final Shape shape;
+    private final AltimeterProfile altimeterProfile;
+    protected final IslandMesh mesh;
+
+    protected AbstractIslandGenerator(
+            IslandMesh mesh,
+            Shape shape,
+            AltimeterProfile altimeterProfile,
+            long seed,
+            int numLakes,
+            int numAquifers,
+            int numRivers
+    ) {
+        this.land = new Land();
+        this.ocean = new Ocean();
+        this.seed = seed;
+        this.rand = new Random(seed);
+        this.mesh = mesh;
+        this.shape = shape;
+        this.altimeterProfile = altimeterProfile;
+        this.numLakes = numLakes;
+        this.numRivers = numRivers;
+        this.numAquifers = numAquifers;
+    }
+
+    @Override
+    public long getSeed() {
+        return this.seed;
+    }
+
+    @Override
+    public final void generate() {
+        List<Tile> tiles = this.mesh.getTiles();
+
+        this.generateIsland(tiles, this.ocean, this.land, this.shape);
+        this.generateElevation(this.rand, this.land);
+        this.generateAquifers(this.rand, this.land, this.numAquifers);
+        this.generateLakes(this.rand, this.land, this.numLakes);
+        this.generateRivers(this.rand, this.land, this.ocean, this.numRivers);
+        this.generateHumidity(this.land);
+    }
+
+    /**
+     * Generates the land tiles and ocean tiles
+     *
+     * @param tiles The {@link List} of tiles given by the mesh
+     */
+    protected abstract void generateIsland(List<Tile> tiles, Ocean ocean, Land land, Shape shape);
+
+    /**
+     * Generates lakes in the island
+     *
+     * @param land All the land {@link Tile} since lakes can only be in land
+     */
+    protected abstract void generateLakes(Random rand, Land land, int numLakes);
+
+    /**
+     *
+     * @param land The {@link Land} of the island
+     * @param ocean The {@link Ocean} of the island
+     * @param numRivers The number of {@link River} to generate
+     */
+    private void generateRivers(Random rand, Land land, Ocean ocean, int numRivers) {
+        RiverGenerator riverGenerator = new RiverGenerator(land, ocean);
+        List<River> rivers = riverGenerator.generate(rand, numRivers);
+        rivers.forEach(land::addRiver);
+    }
+
+    /**
+     * Sets the elevation of land tiles. Ocean tiles are left at 0.
+     */
+    private void generateElevation(Random rand, Land land) {
+        land.getTiles().forEach(t -> {
+            double elevation = this.altimeterProfile.generateElevation(rand, this.shape, t);
+            t.setElevation(elevation);
+        });
+    }
+
+    /**
+     *
+     * @param land The {@link Land} to generate aquifers for
+     * @param numAquifers The number of aquifers to generate
+     */
+    private void generateAquifers(Random rand, Land land, int numAquifers) {
+        List<Tile> tiles = land.getTiles();
+
+        for(int i=0; i < numAquifers; i++) {
+            int randIdx = rand.nextInt(0, tiles.size());
+            Tile aquifer_tile = tiles.get(randIdx);
+            aquifer_tile.putAquifer();
+
+            tiles = tiles.stream().filter(t -> !t.hasAquifer()).toList();
+        }
+    }
+
+    /**
+     *
+     * @param land The {@link Land} to generate humidity for
+     */
+    private void generateHumidity(Land land){
+        // Add humidity from the lakes
+        land.getLakes().forEach(lake -> {
+            lake.getNeighbors().forEach(lake::giveHumidity);
+        });
+
+        // Add humidity from the rivers
+        land.getRivers().forEach(r -> {
+            r.getNeighbors().forEach(r::giveHumidity);
+        });
+
+        // Soil absorption (tiles spread humidity amongst themselves)
+        land.getTiles().stream().filter(t -> t.getType().getGroup() != TileGroup.WATER).forEach(tile -> {
+            tile.getNeighbors().forEach(tile::giveHumidity);
+        });
+
+        // Add (its really remove) humidity from ocean
+        ocean.getNeighbors().forEach(ocean::giveHumidity);
+    }
+}
---
+++ island/src/main/java/ca/mcmaster/cas/se2aa4/a2/island/generator/generators/LagoonIslandGenerator.java
@@ -0,0 +1,44 @@
+package ca.mcmaster.cas.se2aa4.a2.island.generator.generators;
+
+import ca.mcmaster.cas.se2aa4.a2.island.elevation.altimetry.profiles.LagoonAltimeter;
+import ca.mcmaster.cas.se2aa4.a2.island.generator.AbstractIslandGenerator;
+import ca.mcmaster.cas.se2aa4.a2.island.geography.Lake;
+import ca.mcmaster.cas.se2aa4.a2.island.geography.Land;
+import ca.mcmaster.cas.se2aa4.a2.island.geography.Ocean;
+import ca.mcmaster.cas.se2aa4.a2.island.geometry.Shape;
+import ca.mcmaster.cas.se2aa4.a2.island.mesh.IslandMesh;
+import ca.mcmaster.cas.se2aa4.a2.island.tile.Tile;
+import ca.mcmaster.cas.se2aa4.a2.island.tile.type.TileType;
+
+import java.util.List;
+import java.util.Random;
+
+public class LagoonIslandGenerator extends AbstractIslandGenerator {
+    public LagoonIslandGenerator(IslandMesh mesh, Shape shape, long seed, int numAquifers, int numRivers) {
+        super(mesh, shape, new LagoonAltimeter(), seed, 1, numAquifers, numRivers);
+    }
+
+    @Override
+    protected void generateIsland(List<Tile> tiles, Ocean ocean, Land land, Shape shape) {
+        List<Tile> oceanTiles = tiles.stream().filter(t -> !shape.contains(t)).toList();
+        ocean.addAllTiles(oceanTiles);
+
+        List<Tile> landTiles = tiles.stream().filter(shape::contains).toList();
+        land.addAllTiles(landTiles);
+        land.getTiles().stream().filter(t ->
+                t.getNeighbors().stream().anyMatch(t1 -> t1.getType() == TileType.OCEAN_TILE)
+        ).forEach(t -> t.setType(TileType.BEACH_TILE));
+    }
+
+    @Override
+    protected void generateLakes(Random random, Land land, int numLakes) {
+        List<Tile> mainLandTiles = land.getTiles();
+        List<Tile> lakeTiles = mainLandTiles.stream().filter(t -> t.getElevation() < 0.2).toList();
+        Lake lake = new Lake(lakeTiles.get(0));
+        lake.addAllTiles(lakeTiles.subList(1, lakeTiles.size()));
+        lake.setElevation(0.2);
+        land.addLake(lake);
+
+        lake.getNeighbors().forEach(t -> t.setType(TileType.BEACH_TILE));
+    }
+}
