7f35d698821a7a314a06759793d7aef25ed6d951
Abdallah Alqashqish <alqashqa@mcmaster.ca>
2023-03-26 11:18:23 -0400

    F17: Add subtropical desert to the tropical biome

--- island/src/main/java/ca/mcmaster/cas/se2aa4/a2/island/biome/biomes/TropicalBiome.java
+++ island/src/main/java/ca/mcmaster/cas/se2aa4/a2/island/biome/biomes/TropicalBiome.java
@@ -7,7 +7,8 @@ import ca.mcmaster.cas.se2aa4.a2.island.tile.type.TileType;
 public class TropicalBiome extends AbstractBiome {
     public TropicalBiome() {
         super();
-        super.addRule(new Rule(0, 50, 30, 50, TileType.BEACH));
+        super.addRule(new Rule(0, 1, 30, 50, TileType.SUBTROPICAL_DESERT));
+        super.addRule(new Rule(1, 50, 30, 50, TileType.BEACH));
         super.addRule(new Rule(50, 200, 30, 50, TileType.TROPICAL_SEASONAL_FOREST));
         super.addRule(new Rule(200, 500, 30, 50, TileType.TROPICAL_RAIN_FOREST));
         super.addRule(new Rule(0, 200, 0, 30, TileType.TROPICAL_SEASONAL_FOREST));
