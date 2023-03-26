f8adc2a7ee26e9da8c6ce62c2a980862eb13a5a5
Abdallah Alqashqish <alqashqa@mcmaster.ca>
2023-03-25 22:04:50 -0400

    F14: Do not add the same lake tile twice

--- island/src/main/java/ca/mcmaster/cas/se2aa4/a2/island/generator/generators/LagoonIslandGenerator.java
+++ island/src/main/java/ca/mcmaster/cas/se2aa4/a2/island/generator/generators/LagoonIslandGenerator.java
@@ -6,7 +6,6 @@ import ca.mcmaster.cas.se2aa4.a2.island.geography.Lake;
 import ca.mcmaster.cas.se2aa4.a2.island.geography.Land;
 import ca.mcmaster.cas.se2aa4.a2.island.geography.Ocean;
 import ca.mcmaster.cas.se2aa4.a2.island.geometry.Shape;
-import ca.mcmaster.cas.se2aa4.a2.island.humidity.profiles.HumidityProfile;
 import ca.mcmaster.cas.se2aa4.a2.island.mesh.IslandMesh;
 import ca.mcmaster.cas.se2aa4.a2.island.tile.Tile;
 import ca.mcmaster.cas.se2aa4.a2.island.tile.type.TileType;
@@ -35,7 +34,7 @@ public class LagoonIslandGenerator extends AbstractIslandGenerator {
         List<Tile> mainLandTiles = land.getTiles();
         List<Tile> lakeTiles = mainLandTiles.stream().filter(t -> t.getElevation() < 0.2).toList();
         Lake lake = new Lake(lakeTiles.get(0));
-        lake.addAllTiles(lakeTiles.subList(0, lakeTiles.size()));
+        lake.addAllTiles(lakeTiles.subList(1, lakeTiles.size()));
         lake.setElevation(0.2);
         land.addLake(lake);
 
