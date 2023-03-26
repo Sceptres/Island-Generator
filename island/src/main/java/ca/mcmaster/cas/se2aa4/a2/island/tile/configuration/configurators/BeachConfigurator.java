8f0ce32009b1dda2bfe1326e5243b8c800e41f6f
Abdallah Alqashqish <alqashqa@mcmaster.ca>
2023-03-25 21:59:18 -0400

    F14: Update humidity handler import packages

--- island/src/main/java/ca/mcmaster/cas/se2aa4/a2/island/tile/configuration/configurators/BeachConfigurator.java
+++ island/src/main/java/ca/mcmaster/cas/se2aa4/a2/island/tile/configuration/configurators/BeachConfigurator.java
@@ -1,7 +1,7 @@
 package ca.mcmaster.cas.se2aa4.a2.island.tile.configuration.configurators;
 
 import ca.mcmaster.cas.se2aa4.a2.island.elevation.handler.handlers.NormalElevationHandler;
-import ca.mcmaster.cas.se2aa4.a2.island.humidity.humidityHandlers.NormalHumidityHandler;
+import ca.mcmaster.cas.se2aa4.a2.island.humidity.handlers.NormalHumidityHandler;
 import ca.mcmaster.cas.se2aa4.a2.island.color.colors.tiles.BeachColorGenerator;
 import ca.mcmaster.cas.se2aa4.a2.island.tile.configuration.AbstractConfigurator;
 
--- island/src/main/java/ca/mcmaster/cas/se2aa4/a2/island/tile/configuration/configurators/OceanConfigurator.java
+++ island/src/main/java/ca/mcmaster/cas/se2aa4/a2/island/tile/configuration/configurators/OceanConfigurator.java
@@ -1,7 +1,7 @@
 package ca.mcmaster.cas.se2aa4.a2.island.tile.configuration.configurators;
 
 import ca.mcmaster.cas.se2aa4.a2.island.elevation.handler.handlers.NoElevationHandler;
-import ca.mcmaster.cas.se2aa4.a2.island.humidity.humidityHandlers.NoHumidityHandler;
+import ca.mcmaster.cas.se2aa4.a2.island.humidity.handlers.NoHumidityHandler;
 import ca.mcmaster.cas.se2aa4.a2.island.color.colors.tiles.OceanColorGenerator;
 import ca.mcmaster.cas.se2aa4.a2.island.tile.configuration.AbstractConfigurator;
 
